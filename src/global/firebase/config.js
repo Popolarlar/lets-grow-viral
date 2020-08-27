ab6535fecac825448faeea434a4eb0cbb5621fdd
jin <potl28326@gmail.com>
2020-08-27 10:38:44 +1000

    Set up firebase

---
+++ src/global/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBSiKCq8gzbu0AcEpHwo7e9IZMvVbP4bKY",
+  authDomain: "grow-viral-app.firebaseapp.com",
+  databaseURL: "https://grow-viral-app.firebaseio.com",
+  projectId: "grow-viral-app",
+  storageBucket: "grow-viral-app.appspot.com",
+  messagingSenderId: "176581386822",
+  appId: "1:176581386822:web:655346983a692e6810a047",
+};
